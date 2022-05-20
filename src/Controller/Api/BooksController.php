5fa979e4c3494ce1158adba48bafc0f7a7cbe2e6
DiegoMarty <contacto@diegomarty.com>
2022-05-20 09:48:03 +0200

    ✨ 5 - Create book with Post

--- src/Controller/Api/BooksController.php
+++ src/Controller/Api/BooksController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Book;
 use App\Repository\BookRepository;
-use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
 
 class BooksController extends AbstractFOSRestController
 {
@@ -16,5 +18,21 @@ class BooksController extends AbstractFOSRestController
         BookRepository $bookRepository,
     ) {
         return $bookRepository->findAll();
+    }
+
+    /**
+     * @Rest\Post(path="/books")
+     * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
+     */
+    public function postActions(
+        EntityManagerInterface $em,
+    ) {
+        $book = new Book();
+
+        $book->setTitle('Titulo del libro POST');
+        $em->persist($book);
+        $em->flush();
+
+        return $book;
     }
 }
