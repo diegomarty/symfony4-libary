71696b57006334f9cd0b630bf9f513d931976f08
DiegoMarty <contacto@diegomarty.com>
2022-05-20 12:21:23 +0200

    ✨ 6 - Formularios

--- src/Controller/Api/BooksController.php
+++ src/Controller/Api/BooksController.php
@@ -3,8 +3,10 @@
 namespace App\Controller\Api;
 
 use App\Entity\Book;
+use App\Form\Type\BookFormType;
 use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 
@@ -14,7 +16,7 @@ class BooksController extends AbstractFOSRestController
      * @Rest\Get(path="/books")
      * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
      */
-    public function getActions(
+    public function getAction(
         BookRepository $bookRepository,
     ) {
         return $bookRepository->findAll();
@@ -24,15 +26,21 @@ class BooksController extends AbstractFOSRestController
      * @Rest\Post(path="/books")
      * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
      */
-    public function postActions(
+    public function postAction(
         EntityManagerInterface $em,
+        Request $request,
     ) {
         $book = new Book();
 
-        $book->setTitle('Titulo del libro POST');
-        $em->persist($book);
-        $em->flush();
+        $form = $this->createForm(BookFormType::class, $book);
+        $form->handleRequest($request);
 
-        return $book;
+        if($form->isSubmitted() && $form->isValid()) {
+            $em->persist($book);
+            $em->flush();
+
+            return $book;
+        }
+        return $form;
     }
 }
---
+++ src/Form/Type/BookFormType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Entity\Book;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class BookFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('title', TextType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+            // Desactivamos la proteccion de CSRF para las API
+            'csrf_protection' => false
+        ]);
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return '';
+    }
+
+    public function getName(): string
+    {
+        return '';
+    }
+
+}
